15427bdc492299dd325e754cdb05426b32639e1a
marc <marclang123@hotmail.com>
2015-01-28 16:29:11 +0100

    Modification de la direction et du nom du fichier en sortie

--- ymraster/ndvi.py
+++ ymraster/ndvi.py
@@ -17,30 +17,24 @@ if __name__ == "__main__":
     "given image, given the index of the red and nir band. Indexation starts" + 
     " at 1.\n Example : python ndvi.py ../../Donnees/Donnes_supp/" +
     "Spot6_MS_31072013.tif 3 4 -pref A -dir data_example_seg") 
-    parser.add_argument("--xs_file", help="Path of the multi-spectral image",
-                        required = True)
-    parser.add_argument("--idx_red", help="Chanel number of the red band",
-                        type = int, required = True)
-    parser.add_argument("--idx_nir", help="Chanel number of the nir band",
-                        type = int, required = True)    
-    parser.add_argument("-pref", "--prefixe", help ="Prefixe to add to the " +
-    "file to be written", default = "", type = str)
-    parser.add_argument("-dir","--dir_file", default = "", help = "Path of "+
-    "the folder where the output will be written" )
+    parser.add_argument("--xs_file","-xs", help="Path of the multi-spectral " +
+                        "image", required = True)
+    parser.add_argument("--idx_red","-red", help="Chanel number of the red " +
+                        "band", type = int, required = True)
+    parser.add_argument("--idx_nir", "-nir", help="Chanel number of the nir " +
+                        "band", type = int, required = True)    
+    parser.add_argument("-out", "--out_file", help ="Name of the output file",
+                        required = True, type = str)
+    parser.add_argument("-d","--dir", default = "", help = "Path of the " +
+                        "folder where the output will be written. The \"/\"" +
+                        " or \"\\\" have to be add at the end.")
     args = parser.parse_args()
-    
-    #Symbol to add in function of the optional parse arguments, to have a 
-    #proper path
-    if args.dir_file:
-        args.dir_file += '/'
-    if args.prefixe:
-        args.prefixe += '_'
-    output_ndvi = args.dir_file + args.prefixe + 'ndvi.tif'
     print args
-    
+
+    output_ndvi = args.dir + args.out_file 
     #set of the instance    
     spot_xs = Raster(args.xs_file)
     
     #Execution of the method
     ndvi_img = spot_xs.ndvi(output_ndvi, args.idx_red, args.idx_nir)
-    
+    print "Writting the ndvi image has been realized succesfully\n" 
