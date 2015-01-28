f2201bc9841399c568e8899fb7acb9f6357a1461
marc <marclang123@hotmail.com>
2015-01-28 16:41:47 +0100

    Modification des arguments de direction et nom du fichier en sortie

--- ymraster/concatenation.py
+++ ymraster/concatenation.py
@@ -20,27 +20,21 @@ if __name__ == "__main__":
                                     "types are different, then everything " +
                                     "will be converted to the default data " +
                                     "type in OTB (_float_ currently).") 
-    parser.add_argument("--im_files",nargs = '+',  help="list of the images " +
-                        "paths.", required = True)
-    parser.add_argument("-pref", "--prefixe", help ="Prefixe to add to the " +
-                        "file to be written", default = "", type = str)
-    parser.add_argument("-dir","--dir_file", default = "", help = "Path of "+
-                        "the folder where the output will be written" )
+    parser.add_argument("--im_files", "-im", nargs = '+',  help="list of the" +
+                        "images paths.", required = True)
+    parser.add_argument("-out", "--out_file", help ="Name of the output file",
+                        required = True, type = str)
+    parser.add_argument("-d","--dir", default = "", help = "Path of the " +
+                        "folder where the output will be written. The \"/\"" +
+                        " or \"\\\" have to be add at the end.")
     args = parser.parse_args()
-    
-    #Symbol to add in function of the optional parse arguments, to have a 
-    #proper path
-    if args.dir_file:
-        args.dir_file += '/'
-    if args.prefixe:
-        args.prefixe += '_'
-    output_concat = args.dir_file + args.prefixe + 'concatenated.tif'
     print args
     
+    output_concat = args.dir + args.out_file 
     #set of the instances    
     rasters = [Raster(im) for im in args.im_files]    
     
     #Execution of the method
     concatenate_images(rasters, output_concat)
-    
+    print "Concatenation has been realized succesfully\n"
     
