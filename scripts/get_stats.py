cd9065fad33dabd52a6beb5dbcf88355d1369fad
marc <marclang123@hotmail.com>
2015-02-04 11:48:01 +0100

    Ajout d'un exécutable pour la méthode get_stats. Fonctionnel et documenté.

--- scripts/get_stats.py
+++ scripts/get_stats.py
@@ -17,20 +17,27 @@ if __name__ == "__main__":
             " The statistics calculated by default are : mean, standard " +\
             "deviation, min, max and the 20, 40, 50, 60, 80th percentiles. " +\
             "The output is an image at the given format that contains n_band"+\
-            " * n_stat_features bands. This method use the GDAL et NUMPY " +\
+            " * n_stat_features bands. This method uses the GDAL et NUMPY " +\
             "library."
     parser = argparse.ArgumentParser(description= desc)
     parser.add_argument("--label_file", "-lab", help="Path of the label image"
                         ,required = True)
     parser.add_argument("--ms_file","-ms", help="Path of the multi spectral" +
-                        "image on wich the statistics are calculated"
+                        " image on wich the statistics are calculated"
                         ,required = True)
     parser.add_argument("-out", "--out_file", help ="Name of the output file",
                         required = True, type = str)
     parser.add_argument("-d","--dir", default = "", help = "Path of the " +
                         "folder where the output will be written.")
-    parser.add_argument("--stats","-s")
-    parser.add_argument("--percentile","-per")
+    parser.add_argument("--stats","-s", default = ["mean","std","min","max",
+                        "per"], choices = ["mean","std","min","max","per"],
+                        help = "List of the statistics to be calculated. By " +
+                        "default, all the features are calculated, i.e. mean,"+
+                        "std, min, max and per.", nargs = '+')
+    parser.add_argument("--percentile","-per", default = [20,40,50,60,80],
+                        type = int, help = "List of the percentile to be " +
+                        "calculated. By default,the percentiles are 20, 40, " +
+                        "50, 60, 80.", nargs = '+')
     parser.add_argument("--format","-f", help = " Format in wich the output "+
                         "image is written. Any formats supported by GDAL"
                         , default = "Gtiff")
@@ -44,7 +51,7 @@ if __name__ == "__main__":
     ms_img = Raster(args.ms_file)
     out_filename = os.path.join(args.dir, args.out_file)
     
-    label_img.get_stat2(ms_img,out_filename, stats = args.stats, percentile = \
+    label_img.get_stat(ms_img,out_filename, stats = args.stats, percentile = \
                         args.percentile, ext = args.format)
     
     print "Obtention of statistics has been realized succesfully\n"
