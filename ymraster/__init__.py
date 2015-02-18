e30d8a4b701164c5f724dda7c0bcc28177eeb384
marc <marclang123@hotmail.com>
2015-02-18 11:00:36 +0100

    Modification pour l'intégration d'une nouvelle bibliothèque, à cérifier

--- ymraster/__init__.py
+++ ymraster/__init__.py
@@ -2,4 +2,5 @@
 
 """ ymraster pacakge """
 from raster import write_file, concatenate_images, temporal_stats, Raster
-from stat_into_roi import *
+from stat_into_roi import get_samples_from_roi, get_samples_from_label_img, decision_tree, pred_error_metrics
+import stat_into_roi
